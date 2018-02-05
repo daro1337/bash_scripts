e2f5e773b4a2c0de00d5dfba4b396372a54bc290
Dariusz Adamczyk <daro@MBP-daro.local>
2018-02-05 22:00:44 +0100

    inet aliases script

---
+++ inet_alias/inetallias.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#iface eth0:1 inet static
+#        address 10.10.4.X
+#        netmask 255.255.255.255
+#auto eth0:1
+
+i=1
+
+cat iplist2.txt |while read line;
+do
+	echo " "
+	echo "iface eth0:$i inet static";
+	echo "        address $line";
+	echo "        netmask 255.255.255.255";
+	echo "auto eth0:$i";
+	((i++))
+done
+
